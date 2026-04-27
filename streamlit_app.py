e04bc51895ccf0144a59217356e97b6333fbe340
Marina Flores Payan <71663779+marinafloresp@users.noreply.github.com>
2026-04-27 15:19:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+from interpretation import *
+from streamlit_option_menu import option_menu
+st.set_page_config(
+    page_title="EMERY",
+    page_icon="bi bi-basket",
+    layout="wide"
+)
 
+add_logo()
+sideBar()
+st.header("Home")
+st.write("---")
